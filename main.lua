45d5e1449b3efe77c75f44c5c6c35aa742b79545
JennyPans <jennypans@outlook.com>
2021-04-24 21:22:34 +0200

    Refactornig game screen

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 ----------------------------------------------
 --- LUDUM DARE 48
 --- Jenny Pans (Fliflifly)
+--- 2021-04-24
 ----------------------------------------------
 
 ----------------------------------------------
@@ -8,18 +9,17 @@
 ----------------------------------------------
 SX = 1
 SY = 1
+DEBUG = false
 ----------------------------------------------
 --- VARIABLES
 ----------------------------------------------
 local musics = {}
 local graphics = {}
-local menu_states = {"title_screen", "game"}
-local menu = ""
 local player = {}
 local animations = {}
-local sprites = {}
-local planets_sprites = {}
-local debug = false
+local screen = {
+    current = "title_screen"
+}
 ----------------------------------------------
 --- ANIMATIONS
 ----------------------------------------------
@@ -64,7 +64,7 @@ local function changeAnimation(sprite, animation_name)
 end
 --------- NEW
 ----------------------------------------------
-local function newSprite(x, y)
+local function newSprite(screen, x, y)
     local sprite = {}
     sprite.x = x
     sprite.y = y
@@ -83,23 +83,25 @@ local function newSprite(x, y)
     sprite.currentAnimation = ""
     sprite.currentTimeAnimation = 0
     sprite.durationAnimation = 1
-    table.insert(sprites, sprite)
+    table.insert(screen, sprite)
     return sprite
 end
 
-local function newPlayer(x, y)
+local function newPlayer(screen, x, y)
     player = newSprite(x, y)
     player.ax = 50
     player.ay = 50
     player.type = "player"
+    return player
 end
 
-local function newPlanet(x, y)
-    local planet = newSprite(x, y)
+local function newPlanet(screen, x, y)
+    local planet = newSprite(screen.sprites, x, y)
     planet.type = "planet"
     planet.durationAnimation = 1
     changeAnimation(planet, "earth")
-    table.insert(planets_sprites, planet)
+    table.insert(screen.planets, planet)
+    return planet
 end
 --------- UPDATE
 ----------------------------------------------
@@ -123,7 +125,7 @@ end
 ----------------------------------------------
 local function drawSprite(sprite)
     love.graphics.setColor(1, 1, 1, 1)
-    if debug then
+    if DEBUG then
         love.graphics.rectangle("fill", sprite.x, sprite.y, sprite.box.w, sprite.box.h)
     else
         if sprite.currentAnimation == "" then
@@ -144,7 +146,7 @@ end
 ----------------------------------------------
 function love.keypressed(key, scancode, isrepeat)
     if key == "escape" then love.event.quit() end
-    if key == "f3" then debug = not debug end
+    if key == "f3" then DEBUG = not DEBUG end
     if menu == menu_states[1] then
         if key == "space" then menu = menu_states[2] end
     elseif menu == menu_states[2] then
@@ -168,11 +170,13 @@ end
 ----------------------------------------------
 --------- LOAD
 ----------------------------------------------
-local function loadMusics(path)
+local function loadMusics()
+    path = "musics/"
     musics["Spacearray"] = love.audio.newSource(path.."Spacearray.ogg", "stream")
 end
 
-local function loadGraphics(path)
+local function loadGraphics()
+    path = "graphics/"
     graphics["earth"] = love.graphics.newImage(path.."earth.png")
 end
 
@@ -180,33 +184,48 @@ local function loadAnimations()
     animations["earth"] = newAnimation(graphics["earth"], 100, 100, 1)
 end
 
-function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    loadMusics("musics/")
-    loadGraphics("graphics/")
-    loadAnimations()
-    newPlayer(0,0)
-    newPlanet(0,0)
-    menu = menu_states[1]
+local function loadScreen()
+    local screen = {sprites = {}, planets={}}
+    return screen
+end
+
+local function loadTitleScreen()
+    screen.current = "title_screen"
+    screen.title_screen = loadScreen()
+    newPlanet(screen.title_screen, 0,0)
+    newPlanet(screen.title_screen, 200,0).durationAnimation = 5
     musics["Spacearray"]:setLooping(true)
     musics["Spacearray"]:play()
+end
+
+local function loadGame()
+    newPlayer(0,0)
+end
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    loadMusics()
+    loadGraphics()
+    loadAnimations()
+    loadTitleScreen()
 end
 --------- UPDATE
 ----------------------------------------------
 function love.update(dt)
-    updateSprites(sprites, dt)
-    if menu == menu_states[2] then
-        updatePlayer(dt)
+    if screen.current == "title_screen" then
+        updateSprites(screen.title_screen.sprites, dt)
+    elseif screen.current == "game" then
+        updateSprites(screen.game.sprites, dt)
     end
 end
 --------- DRAW
 ----------------------------------------------
 function love.draw()
-    if menu == menu_states[1] then
-        drawSprites(planets_sprites)
-    elseif menu == menu_states[2] then
+    if screen.current == "title_screen" then
+        drawSprites(screen.title_screen.sprites)
+    elseif screen.current == "game" then
         love.graphics.setBackgroundColor(0.8, 0.5, 0.5, 1)
-        drawSprites(sprites)
+        drawSprites(screen.game.sprites)
     end
 end
 ----------------------------------------------
